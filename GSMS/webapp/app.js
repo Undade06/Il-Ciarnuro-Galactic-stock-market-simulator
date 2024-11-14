102c847f83c8c6d5b97d221629b8c8dac2c2ede3
Davide Gaiotto <gaiottodavide1@gmail.com>
2024-11-14 09:31:48 +0100

    Added the main page with background image and logo

---
+++ GSMS/webapp/app.js
@@ -0,0 +1,22 @@
+
+/*---SLIDES SYSTEM---*/
+function toSlide(id){
+    document.querySelectorAll("div.slide").forEach(function(e){
+        e.classList.add("hidden")
+        e.classList.remove("visible")
+        e.querySelectorAll("*").forEach(function(e2){
+            e2.tabIndex="-1"
+        })
+    })
+    let d=document.getElementById(id)
+    d.classList.add("visible")
+    d.classList.remove("hidden")
+    d.querySelectorAll("*").forEach(function(e2){
+        e2.tabIndex="-1"
+    })
+}
+
+function getCurrentSlide(){
+    let s=document.getElementsByClassName("slide visible");
+    if(s.length===0) return null; else return s[0];
+}

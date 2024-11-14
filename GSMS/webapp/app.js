71fbf9e7650ebb4f5bb23bca9668fc43ef6106ec
Davide Gaiotto <gaiottodavide1@gmail.com>
2024-11-14 12:20:13 +0100

    Fixed toSlide function and logo font

--- GSMS/webapp/app.js
+++ GSMS/webapp/app.js
@@ -1,20 +1,20 @@
 
-/*---SLIDES SYSTEM---*/
-function toSlide(id){
-    document.querySelectorAll("div.slide").forEach(function(e){
-        e.classList.add("hidden")
-        e.classList.remove("visible")
-        e.querySelectorAll("*").forEach(function(e2){
-            e2.tabIndex="-1"
-        })
-    })
-    let d=document.getElementById(id)
-    d.classList.add("visible")
-    d.classList.remove("hidden")
-    d.querySelectorAll("*").forEach(function(e2){
-        e2.tabIndex="-1"
-    })
+function toSlide(id) {
+    document.querySelectorAll("div.slide").forEach(function(e) {
+        e.classList.add("hidden");
+        e.classList.remove("visible");
+        e.querySelectorAll("*").forEach(function(e2) {
+            e2.tabIndex = "-1"; 
+        });
+    });
+    let d = document.getElementById(id);
+    d.classList.add("visible");
+    d.classList.remove("hidden");
+    d.querySelectorAll("*").forEach(function(e2) {
+        e2.tabIndex = "0";  
+    });
 }
+
 
 function getCurrentSlide(){
     let s=document.getElementsByClassName("slide visible");

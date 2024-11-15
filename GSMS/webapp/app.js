f566226c3c1396fd54cf0fb94fedb784aa1f2ac5
Matteo Brandi <matt3o.brandi@gmail.com>
2024-11-15 17:59:47 +0100

    Added mulberry32, getValue and support func

--- GSMS/webapp/app.js
+++ GSMS/webapp/app.js
@@ -1,43 +1,74 @@
 
 /*---SLIDES SYSTEM---*/
-function toSlide(id){
-    document.querySelectorAll("div.slide").forEach(function(e){
+function toSlide(id) {
+    document.querySelectorAll("div.slide").forEach(function (e) {
         e.classList.add("hidden")
         e.classList.remove("visible")
-        e.querySelectorAll("*").forEach(function(e2){
-            e2.tabIndex="-1"
+        e.querySelectorAll("*").forEach(function (e2) {
+            e2.tabIndex = "-1"
         })
     })
-    let d=document.getElementById(id)
+    let d = document.getElementById(id)
     d.classList.add("visible")
     d.classList.remove("hidden")
-    d.querySelectorAll("*").forEach(function(e2){
-        e2.tabIndex="-1"
+    d.querySelectorAll("*").forEach(function (e2) {
+        e2.tabIndex = "-1"
     })
 }
 
 
-function getCurrentSlide(){
-    let s=document.getElementsByClassName("slide visible");
-    if(s.length===0) return null; else return s[0];
+function getCurrentSlide() {
+    let s = document.getElementsByClassName("slide visible");
+    if (s.length === 0) return null; else return s[0];
 }
 
-function Stock(name,description,baseValue,variability,volatility,noisiness,influenceability){
-    this.name=name
-    this.description=description
-    this.type="stock"
-    this._baseValue=baseValue
-    this._variability=variability
-    this._volatility=volatility
-    this._noisiness=noisiness
-    this._influenceability=influenceability
-    this._baseF=volatility/100
-    this._influenceability=influenceability
+// All these are supposed
+const MAXSTOCKVALUE = 100000
+const TIMESTEP = 0.01       // Second
+
+//mulberry32 seeded random number generator
+function mulberry32(a) {
+    let t = a += 0x6D2B79F5
+    t = Math.imul(t ^ t >>> 15, t | 1)
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61)
+    return ((t ^ t >>> 14) >>> 0) / 4294967296
 }
 
-Stock.prototype={
-    constructor:Stock,
-    getValue:function(t){
-        
+function Stock(name, description, baseValue, growth, volatility) {
+    this.name = name
+    this.description = description
+    this.type = "stock"
+    this._baseValue = baseValue
+    this.growth = growth
+    this.volatility = volatility
+}
+
+Stock.prototype = {
+    constructor: Stock,
+    getValue: function (t) {
+        return baseValue*Math.exp((this.growth-(Math.pow(this.volatility, 2)/2))*t + this.wiener(t))
+    },
+    wiener: function (t) {
+        let n = Math.floor(t / TIMESTEP),         // Step number
+            z,                                    // Standard normal random variable
+            dw,                                   // Process increment
+            w = [],
+            sum = 0
+            w[0] = 0
+
+         for (let i = 0; i < n; i++) {
+            z = ndn()
+            dw = Math.sqrt(TIMESTEP) * z
+            w.push(w[i] + dw)
+            sum += w[i]
+        }
+
+        return sum
+    },
+    ndn: function(){                    // Generate a random number with standard normal distribution 
+        let u = 0, v = 0;
+        while (u === 0) u = Math.random();              // To avoid zero zero
+        while (v === 0) v = Math.random();
+        return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
     }
 }

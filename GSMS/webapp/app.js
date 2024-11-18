94c044bd9220eda6a60809a7e2c8f92de6407965
Matteo Brandi <matt3o.brandi@gmail.com>
2024-11-18 12:59:35 +0100

    Cleaned code

--- GSMS/webapp/app.js
+++ GSMS/webapp/app.js
@@ -46,25 +46,20 @@ function Stock(name, description, baseValue, growth, volatility) {
 Stock.prototype = {
     constructor: Stock,
     getValue: function (t) {
-        if (t === "undefined") t = 2
-        return baseValue*Math.exp((this.growth-(Math.pow(this.volatility, 2)/2))*t + this.wiener(t))
+        return baseValue*Math.exp(((this.growth-(Math.pow(this.volatility, 2)/2))*t) + this.volatility*this.wiener(t))
     },
     wiener: function (t) {
         let n = Math.floor(t / TIMESTEP),         // Step number
-            z,                                    // Standard normal random variable
-            dw,                                   // Process increment
-            w = [],
-            sum = 0
-            w[0] = 0
-
-         for (let i = 0; i < n; i++) {
-            z = ndn()
-            dw = Math.sqrt(TIMESTEP) * z
-            w.push(w[i] + dw)
-            sum += w[i]
-        }
-
-        return sum
+        z,                                      // Standard normal random variable
+        dw,                                     // Process increment
+        sum = 0
+    
+     for (let i = 0; i < n; i++) {
+        z = ndn()
+        dw = Math.sqrt(TIMESTEP) * z
+        sum += dw
+    }
+    return Math.max(-3, Math.min(3, sum))           //Limit Wiener to the range -3, 3
     },
     ndn: function(){                    // Generate a random number with standard normal distribution 
         let u = 0, v = 0;
@@ -72,45 +67,4 @@ Stock.prototype = {
         while (v === 0) v = Math.random()
         return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
     }
-}
-
-function prova(){
-    let v = []
-    for(let i = 0; i < 20; i++){
-        v.push(getValueP(i, 50, 0.01, 0.01))
-        console.log(v[i])
-    }
-    console.log(v)
-}
-
-function getValueP(t, b, g, v) {
-    if (t === "undefined") t = 2
-    let exponential, variability=(g-(Math.pow(v, 2)/2))*t, wiener = v*wienerP(t)
-    exponential = (Math.exp(variability + wiener))
-    console.log('Wiener: '+wiener)
-    console.log('Variability: '+variability)
-    console.log('exponential: '+exponential)
-    return b*exponential
-}
-
-function wienerP(t) {
-    let n = Math.floor(t / TIMESTEP),         // Step number
-        z,                                    // Standard normal random variable
-        dw,                                   // Process increment
-        sum = 0
-    
-     for (let i = 0; i < n; i++) {
-        z = ndnP()
-        dw = Math.sqrt(TIMESTEP) * z
-        sum += dw
-    }
-
-    return Math.max(-3, Math.min(3, sum))           //Limit Wiener to the range -3, 3
-}
-
-function ndnP(){                    // Generate a random number with standard normal distribution 
-    let u = 0, v = 0;
-    while (u === 0) u = Math.random()               // To avoid zero
-    while (v === 0) v = Math.random()
-    return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
 }

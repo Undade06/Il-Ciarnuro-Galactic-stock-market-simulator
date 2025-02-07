a9285c5bc4485b4290bc1cea03038a5bfd70ce54
Matteo Brandi <matt3o.brandi@gmail.com>
2025-02-07 11:49:49 +0100

    Added bestStock func

--- GSMS/webapp/api.php
+++ GSMS/webapp/api.php
@@ -30,7 +30,7 @@
                             $_SESSION["user_id"] = $row["username"];
                             $ret = ["error" => 0, "msg" => "Logged in successfully"];
                         } else {
-                            $ret = ["error" => 1, "msg" => "incorrect username or password"];
+                            $ret = ["error" => 1, "msg" => "Incorrect username or password"];
                         }
                     }
                 } break;
@@ -52,7 +52,7 @@
                         $q->execute();
                         $ret = ["error" => 0, "msg" => "Registered successfully"];
                     } else {
-                        $ret = ["error" => 1, "msg" => "Missing fields"];
+                        $ret = ["error" => 1, "msg" => "Missing field/s"];
                         break;
                     }
                 } break;
